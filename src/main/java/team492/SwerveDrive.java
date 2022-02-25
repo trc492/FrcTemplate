d0f27e2c2653d99b0552a65c97cf6200bea62a2e
trc492 <trc492@live.com>
2022-02-24 17:41:54 -0800

    Updated CommonLib. Updated FrcLib. Fixed build break.

--- src/main/java/team492/SwerveDrive.java
+++ src/main/java/team492/SwerveDrive.java
@@ -363,7 +363,7 @@ public class SwerveDrive
         TrcUtil.sleep(50);
 
         robot.globalTracer.traceInfo(
-            funcName, "Module=%s, Zero=%d, PwmPos=%d, quadPos=%d, selectedPos=%d",
+            funcName, "Module=%s, Zero=%d, PwmPos=%d, quadPos=%d, selectedPos=%.0f",
             name, steerZero, steer.motor.getSensorCollection().getPulseWidthPosition(),
             steer.motor.getSensorCollection().getQuadraturePosition(), steer.motor.getSelectedSensorPosition());
 
